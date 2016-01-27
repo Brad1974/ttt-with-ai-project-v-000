5be979d29c8c6da5e94bf7654e7ade1f009802ed
pajamaw <pjpeterww@gmail.com>
2016-01-27 11:11:00 -0500

    Added 3 difficulties with same methods - to be updated with more depth for increased strategic ability.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -78,3 +78,43 @@ class Player::Computer_medium <Player
 
 
 end
+
+class Player::Computer_hard <Player
+
+  attr_accessor :board
+
+ WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def move(board)
+    if    !board.taken?("5")
+      "5"
+    elsif !board.taken?("1")
+      "1"
+    elsif !board.taken?("3")
+      "3"
+    elsif !board.taken?("7")
+      "7"
+    elsif !board.taken?("9")
+      "9"
+    elsif !board.taken?("2")
+      "2"
+    elsif !board.taken?("4")
+      "4"
+    elsif !board.taken?("6")
+      "6"
+    elsif !board.taken?("8")
+      "8"
+    end
+  end  
+
+
+end
